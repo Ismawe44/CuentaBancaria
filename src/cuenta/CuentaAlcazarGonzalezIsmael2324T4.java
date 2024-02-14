1fdd0556b494ef5bb929d5d503250280fb8c1fc5
ismael <ismael@ISMAELTOSHIBAL750.home>
2024-02-14 23:18:04 +0100

    "Mi proyecto CuentaAlcazarGonzalezIsmael2324 comentado con javadoc"

--- src/cuenta/CuentaAlcazarGonzalezIsmael2324T4.java
+++ src/cuenta/CuentaAlcazarGonzalezIsmael2324T4.java
@@ -1,20 +1,43 @@
 package cuenta;
-
+/**
+ *Clase java del programa que define el objeto cuenta con unos parametros y realiza
+ * acciones de ingresar y retirar dinero.
+ * @author Ismael Alcázar González
+ * @since 25/02/2024
+ * @version 1.0 25/02/2024
+ */
 
 public class CuentaAlcazarGonzalezIsmael2324T4 {
 
-
-        private String nombre;
+/**
+ * Categoria de nombre cliente.
+ */
+    private String nombre;
+ /**
+ * Categoria de numero de cuenta
+ */   
     private String cuenta;
+ /**
+ * Categoria de saldo
+ */
     private double saldo;
+ /**
+ * Categoria de tipo interés
+ */
     private double tipoInteres;
 
-   
+   //CONTRUCTORES
     public CuentaAlcazarGonzalezIsmael2324T4 ()
     {
     }
    
-   
+   /**
+    * Constructor para iniciar todas las propiedades de la clase cuenta
+    * @param nom nombre cliente
+    * @param cue numero de cuenta
+    * @param sal saldo real
+    * @param tipo tipo de interés real
+    */
     public CuentaAlcazarGonzalezIsmael2324T4 (String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -23,32 +46,45 @@ public class CuentaAlcazarGonzalezIsmael2324T4 {
         tipoInteres=tipo;
     }
    
-    
+    /**
+     *Metodo para asignar nombre de la cuenta
+     */
     public void asignarNombre(String nom)
     {
         setNombre(nom);
     }
-   
-   
+  /**
+   * Metodo que devuelve el nombre de cuenta
+     */ 
+   /**
+    *Metodo que me devuelve el nombre del propietario de la cuenta
+    */
     public String obtenerNombre()
     {
         return getNombre();
     }
-
-    
+ /**
+    *Metodo que me devuelve el saldo de la cuenta
+    */
      public double estado ()
     {
         return getSaldo();
     }
 
-   
+   /**
+     * Metodo para ingresar dinero que modifica la cantidad 
+     *del saldo disponible
+         */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+ /**
+     * Metodo para retirar dinero que modifica la cantidad 
+     * del saldo disponible
+         */
     
     public void retirar (double cantidad) throws Exception
     {
@@ -58,7 +94,10 @@ public class CuentaAlcazarGonzalezIsmael2324T4 {
             throw new Exception ("No hay suficiente saldo");
         setSaldo(getSaldo() - cantidad);
     }
-
+/**
+ * Devuelve la cantidad en cuenta
+ * @return getCuenta()
+ */
     
     public String obtenerCuenta ()
     {
--- src/cuenta/Main.java
+++ src/cuenta/Main.java
@@ -1,23 +1,34 @@
 package cuenta;
-
+/**
+ * Clase que realiza una serie de operaciones (ingresar y retirar dinero)
+ * resultado en pantalla con System out.println
+ * @author Ismael Alcázar González
+ *  @since 25/02/2024
+ * @version 1.0 25/02/2024
+ */
 
 public class Main {
-
+/**
+ * Metodo que crea un objeto CuentaAlcazarGonzalezIsmael2324T4 que realiza un saldo inicial
+ * luego un ingreso de dinero y finalmente una retirada de dinero imprimiendo por pantalla,
+ * el saldo Actual.
+ */
     
     public static void main(String[] args) {
         CuentaAlcazarGonzalezIsmael2324T4 miCuentaAlcazarGonzalezIsmael2324T4;
-        double saldoActual;
+        double saldoActual;// saldo Actual
         int x;
         
-        miCuentaAlcazarGonzalezIsmael2324T4 = new CuentaAlcazarGonzalezIsmael2324T4("Antonio Sánchez","ES55-3058-2365-8522-1234-5678",2500,0);
-        System.out.println("Saldo inicial de la cuenta: "+miCuentaAlcazarGonzalezIsmael2324T4.estado()+"€");
-        retiraDineroAlcazarGonzalezIsmael(miCuentaAlcazarGonzalezIsmael2324T4);
+        miCuentaAlcazarGonzalezIsmael2324T4 = new CuentaAlcazarGonzalezIsmael2324T4("Antonio Sánchez","ES55-3058-2365-8522-1234-5678",2500,0);//constructor
+        System.out.println("Saldo inicial de la cuenta: "+miCuentaAlcazarGonzalezIsmael2324T4.estado()+"€");// imprime por pantalla el saldo actual.
+        retiraDineroAlcazarGonzalezIsmael(miCuentaAlcazarGonzalezIsmael2324T4);// retirada de dinero double
         
-        ingresaDineroAlcazarGonzalezIsmael(miCuentaAlcazarGonzalezIsmael2324T4, "ingreso en cuenta");
-        saldoActual = miCuentaAlcazarGonzalezIsmael2324T4.estado();
-        System.out.println("El saldo actual es "+ saldoActual );
+        ingresaDineroAlcazarGonzalezIsmael(miCuentaAlcazarGonzalezIsmael2324T4, "ingreso en cuenta");// ingreso de dinero double
+        saldoActual = miCuentaAlcazarGonzalezIsmael2324T4.estado();// introduce el saldo actual en variable double
+        System.out.println("El saldo actual es "+ saldoActual );// imprime por pantalla el saldo Actual
     }
 
+ /* Metodo para ingresar dinero en cuenta utilizando el try y catch*/
     private static void ingresaDineroAlcazarGonzalezIsmael(CuentaAlcazarGonzalezIsmael2324T4 miCuentaAlcazarGonzalezIsmael2324T4, String concepto) {
         int x;
         try
@@ -30,7 +41,7 @@ public class Main {
             System.out.print("Fallo al ingresar");
         }
     }
-
+/* Metodo para retirar dinero en cuenta utilizando el try y catch*/
     private static void retiraDineroAlcazarGonzalezIsmael(CuentaAlcazarGonzalezIsmael2324T4 miCuentaAlcazarGonzalezIsmael2324T4) {
         int x;
         try
